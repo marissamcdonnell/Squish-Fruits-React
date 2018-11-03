b9c96f1cdb7f1a4ca2e13f966c4671340164a4d1
Marissa McDonnell <marissa.mcdonnell@fansided.com>
2018-11-03 13:39:22 -0700

    Add appContext

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppRouter from './router/AppRouter';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import AppRouter from "./router/AppRouter";
+import { AppContextProvider } from "./utils/AppContext";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
 
-ReactDOM.render(<AppRouter />, document.getElementById("app"));
+const App = () => <AppContextProvider render={appContext => <AppRouter />} />;
+
+ReactDOM.render(<App />, document.getElementById("app"));
--- src/components/List.js
+++ src/components/List.js
@@ -1,35 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import React from "react";
+import { Link } from "react-router-dom";
+import { withAppContext } from "../utils/AppContext";
+import PropTypes from "prop-types";
 
-export default class List extends React.Component {
-    state = {
-        fruits: []
-    }
+const List = ({ appContext }) => (
+  <div className="container">
+    <div className="side">
+      <ul>
+        {appContext.fruits.map(fruit => (
+          <li key={fruit.id}>
+            <img id={fruit.id} src={fruit.url} />
+            {fruit.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+    <div className="yellowbox instructions">
+      <div className="front-page-text">
+        <h3>
+          Now that you have seen our list of squishables, it is time to start
+          the squishing process.
+        </h3>
+        <p>Please click the button below to begin.</p>
+        <Link to="/fruits">
+          <button>Squishables</button>
+        </Link>
+      </div>
+    </div>
+  </div>
+);
 
-    componentDidMount() {
-        axios.get(`https://api.myjson.com/bins/8t7qv`).then(res => {
-            this.setState({ fruits: res.data.data });
-        })
-    }
+List.propTypes = {
+  appContext: PropTypes.object.isRequired
+};
 
-    render() {
-        return (
-        <div className="container">
-            <div className = "side">
-                <ul>
-                    {this.state.fruits.map( fruit => <li key={fruit.id}><img id={fruit.id} src={fruit.url} />{fruit.name}</li>)}
-                </ul>
-            </div>
-
-            <div className = "yellowbox instructions">
-                <div className="front-page-text">
-                    <h3>Now that you have seen our list of squishables, it is time to start the squishing process.</h3>
-                    <p>Please click the button below to begin.</p>
-                    <Link to="/fruits"><button>Squishables</button></Link>
-                </div>
-            </div>
-        </div>
-        )
-    }
-}
+export default withAppContext(List);
